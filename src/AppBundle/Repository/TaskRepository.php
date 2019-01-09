904c810043473108aca1269e088d53d325a71f84
yohannzaoui <yohannzaoui@gmail.com>
2019-01-09 15:55:08 +0100

    add repository class

---
+++ src/AppBundle/Repository/TaskRepository.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yohann Zaoui
+ * Date: 09/01/2019
+ * Time: 14:40
+ */
+
+namespace AppBundle\Repository;
+
+
+use AppBundle\Entity\Task;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+
+/**
+ * Class TaskRepository
+ * @package AppBundle\Repository
+ */
+class TaskRepository extends ServiceEntityRepository
+{
+    /**
+     * TaskRepository constructor.
+     * @param RegistryInterface $registry
+     */
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Task::class);
+    }
+
+    /**
+     * @param $task
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function save($task)
+    {
+        $this->_em->persist($task);
+        $this->_em->flush();
+    }
+
+    /**
+     * @param $task
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function delete($task)
+    {
+        $this->_em->remove($task);
+        $this->_em->flush();
+    }
+
+    /**
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function update()
+    {
+        $this->_em->flush();
+    }
+}
---
+++ src/AppBundle/Repository/UserRepository.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yohann Zaoui
+ * Date: 09/01/2019
+ * Time: 13:31
+ */
+
+namespace AppBundle\Repository;
+
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\Security\User\UserLoaderInterface;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use AppBundle\Entity\User;
+
+/**
+ * Class UserRepository
+ * @package AppBundle\Repository
+ */
+class UserRepository extends ServiceEntityRepository implements UserLoaderInterface
+{
+    /**
+     * UserRepository constructor.
+     * @param RegistryInterface $registry
+     */
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+
+    /**
+     * @param string $username
+     * @return null|\Symfony\Component\Security\Core\User\UserInterface|void
+     */
+    public function loadUserByUsername($username)
+    {
+        // TODO: Implement loadUserByUsername() method.
+    }
+
+    /**
+     * @param $user
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function save($user)
+    {
+        $this->_em->persist($user);
+        $this->_em->flush();
+    }
+
+    /**
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function update()
+    {
+        $this->_em->flush();
+    }
+
+    /**
+     * @param $user
+     * @throws \Doctrine\ORM\ORMException
+     * @throws \Doctrine\ORM\OptimisticLockException
+     */
+    public function delete($user)
+    {
+        $this->_em->remove($user);
+        $this->_em->flush();
+    }
+
+}
