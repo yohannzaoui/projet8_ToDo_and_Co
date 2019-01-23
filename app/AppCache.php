6c0af6c2c4e1a34d4b0b79c5ede579d83c3d5bb5
yohannzaoui <yohannzaoui@gmail.com>
2019-01-23 14:49:44 +0100

    remove swiftmailer bundle

--- app/AppCache.php
+++ app/AppCache.php
@@ -1,7 +1,14 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: Yohann Zaoui
+ * Date: 22/01/2019
+ * Time: 21:13
+ */
 
 use Symfony\Bundle\FrameworkBundle\HttpCache\HttpCache;
 
 class AppCache extends HttpCache
 {
+
 }
