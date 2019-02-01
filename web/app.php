9ce1c715f57c3a2412597f39cb6b7fd383d69f42
yohannzaoui <yohannzaoui@gmail.com>
2019-02-01 17:42:55 +0100

    add cache for autoload

--- web/app.php
+++ web/app.php
@@ -1,10 +1,14 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\ClassLoader\ApcClassLoader;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
+
+$loader = new ApcClassLoader('sf', $loader);
+$loader->register(true);
 
 $kernel = new AppKernel('prod', false);
 //$kernel->loadClassCache();
