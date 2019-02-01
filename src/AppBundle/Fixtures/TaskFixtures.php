eda1ca9339b7c5697a4ed2df849445e0fd0e55c1
yohannzaoui <yohannzaoui@gmail.com>
2019-02-01 17:43:55 +0100

    add fixtures for anonymous task

---
+++ src/AppBundle/Fixtures/TaskFixtures.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Yohann Zaoui
+ * Date: 01/02/2019
+ * Time: 14:43
+ */
+
+namespace AppBundle\Fixtures;
+
+
+use AppBundle\Entity\Task;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+/**
+ * Class TaskFixtures
+ * @package AppBundle\Fixtures
+ */
+class TaskFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     * @throws \Exception
+     */
+    public function load(ObjectManager $manager)
+    {
+        for ($i = 0; $i < 20; $i++) {
+            $task = new Task();
+            $task->setTitle('Tâche anonyme '.$i);
+            $task->setContent('Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+            $manager->persist($task);
+        }
+
+        $manager->flush();
+    }
+
+}
