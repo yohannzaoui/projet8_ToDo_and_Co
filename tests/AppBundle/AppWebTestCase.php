296e72aacbc6cc26810fb52611b04e87d49b941d
yohannzaoui <yohannzaoui@gmail.com>
2019-02-03 16:01:02 +0100

    update

--- tests/AppBundle/AppWebTestCase.php
+++ tests/AppBundle/AppWebTestCase.php
@@ -34,10 +34,23 @@ class AppWebTestCase extends WebTestCase
     }
 
 
-    /**
-     *
-     */
+
     protected function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+        $em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
+        $user = $em->getRepository(User::class)->findOneBy(['username'=>'admin']);
+        //$user = 'admin';
+
+        $token = new UsernamePasswordToken($user, null, 'main', ['ROLE_ADMIN']);
+        $session->set('_security_'.'main', serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
+
+    protected function logInUser()
     {
         $session = $this->client->getContainer()->get('session');
         //$em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,52 +3,14 @@
 namespace Tests\AppBundle\Controller;
 
 
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-//use Tests\AppBundle\AppWebTestCase;
-use AppBundle\Entity\User;
-use Symfony\Component\BrowserKit\Cookie;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Tests\AppBundle\AppWebTestCase;
 
 /**
  * Class DefaultControllerTest
  * @package Tests\AppBundle\Controller
  */
-class DefaultControllerTest extends WebTestCase
+class DefaultControllerTest extends AppWebTestCase
 {
-
-    /**
-     * @var
-     */
-    private $client;
-
-    /**
-     *
-     */
-    public function setUp()
-    {
-        $this->client = static::createClient();
-    }
-
-
-    /**
-     *
-     */
-    private function logIn()
-    {
-        $session = $this->client->getContainer()->get('session');
-        //$em = $this->client->getContainer()->get('doctrine.orm.entity_manager');
-        $user = 'admin';
-
-        $token = new UsernamePasswordToken($user, null, 'main', ['ROLE_ADMIN']);
-        $session->set('_security_'.'main', serialize($token));
-        $session->save();
-
-        $cookie = new Cookie($session->getName(), $session->getId());
-        $this->client->getCookieJar()->set($cookie);
-    }
-
-
-
 
     /**
      *
@@ -66,7 +28,7 @@ class DefaultControllerTest extends WebTestCase
      */
     public function testHomeIfLogin()
     {
-        $this->logIn();
+        $this->logInUser();
 
         $crawler = $this->client->request('GET', '/');
 
